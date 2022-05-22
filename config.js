ec5ab68f318817b76978680213425762a6197908
naresh63 <nvalvi63@gmail.com>
2022-05-22 17:46:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const DB_URL='mongodb+srv://nareshskystisproject1:EKzlDdsAxHni9nmu@cluster0.wqfpz.mongodb.net/Natours?retryWrites=true&w=majority'
+
 // local 
 const DB_LOCAL="mongodb://localhost:27017/e-commerce"
 mongoose.connect(DB_LOCAL)
