023355f5e5df8ed5a8150a2702282062b7e9ad82
naresh63 <nvalvi63@gmail.com>
2022-05-23 12:19:53 +0530

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-
-// local 
-const DB_LOCAL="mongodb://localhost:27017/e-commerce"
-mongoose.connect(DB_LOCAL)
-.then(()=>console.log("connected to local db"))
-.catch(err=>console.log(err))
